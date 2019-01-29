8f6f85f65a8fbebb44ce3d2e909cec37d31901ff
ravindrareddy2 <ravindrareddy1729@gmail.com>
2019-01-29 09:56:55 +0530

    changes

--- application.py
+++ application.py
@@ -1,15 +1,48 @@
 import os
 
-from flask import Flask, render_template, request, jsonify, Response
-import random, json, time, datetime
-from flask_socketio import SocketIO, emit, join_room, leave_room
-
+from datetime import datetime
+from flask import Flask, render_template, session, request, redirect, url_for, jsonify
+from flask_session import Session
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+
+#configuring socketio
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+#list to store chatroom names
+chatlist = [] 
 
+#list to store current users
+usernames = []
+
+#dictionary to store his/her message and time
+messages = {}
+
+#first page to display
 @app.route("/")
-def index():
-    return "Project 2: TODO"
+def index():  
+    return render_template("index.html")
+
+#after entering name, take user to chat room
+@app.route("/chatroom", methods=["POST"])
+def chatroom():
+   username = request.form.get("user_name")
+   return render_template("channels.html", user_name=username)
+
+#c
+@socketio.on("submit channel")
+def new_channel(data):
+   selection = data["selection"]
+   chatlist.append(selection)
+   emit("announce channel", {"selection": data["selection"]}, chatlist=chatlist, broadcast=True)
+
+
+
+   
+
+
+
+
+
--- static/index.js
+++ static/index.js
@@ -1,4 +1,29 @@
-document.addEventListener(DOMContentLoaded, ()=>{
-    var socket = io.connect('http://' + document.domain + ':' + location.port);
-    
+document.addEventListener("DOMContentLoaded",()=>{
 
+    //connect to web socket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    //on connected to web socket
+    socket.on("connect",() => {
+
+        //button to emit submit channel
+        document.querySelector("button").onclick = () =>{
+            const selection = document.getElementById("#newchannel").value;
+            socket.emit("submit channel", {"selection": selection})
+
+        }
+
+
+    })
+
+    //on submiting channel
+    socket.on("announce channel", data => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `<a href="/chatroom/${data["chatlist"]}"> ${data["selection"]} </a>`;
+        console.log(li.innerHTML);
+        document.querySelector("#chatlist").append(li);
+
+    })
+
+})
