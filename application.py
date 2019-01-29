31a5330e43621551adefa676bdff71810af62f8c
ravindrareddy2 <ravindrareddy1729@gmail.com>
2019-01-29 09:50:01 +0530

    Added standard HTML page tags

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 import os
 
-from flask import Flask
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request, jsonify, Response
+import random, json, time, datetime
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
---
+++ static/index.js
@@ -0,0 +1,4 @@
+document.addEventListener(DOMContentLoaded, ()=>{
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    
+
